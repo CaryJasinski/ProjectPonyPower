cbe9064ee18df903fd2d8a98aedead117872bfd0
Cary Jasinski <cary.jasinski@loop.colum.edu>
2015-04-13 22:52:09 -0500

    Initial Commit - Unity Project
    Created repository and added the existing Unity 5 project.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float movementSpeed = 5f;
+	Rigidbody2D playerRigidbody;
+	float moveSpeed = 0;
+
+	void Start () 
+	{
+		moveSpeed = movementSpeed;
+		playerRigidbody = GetComponent<Rigidbody2D>();
+	}
+
+	void Update () 
+	{
+		HandleInput();
+	}
+
+	void MovePlayer()
+	{
+		//Vector2 player
+	}
+
+	void HandleInput()
+	{
+		Vector2 movementDirection = Vector2.zero;
+		if(Input.GetKey(KeyCode.W))
+		{
+			movementDirection += new Vector2(0,1);
+		}
+		if(Input.GetKey(KeyCode.A))
+		{
+			movementDirection += new Vector2(-1,0);
+		}
+		if(Input.GetKey(KeyCode.S))
+		{
+			movementDirection += new Vector2(0,-1);
+		}
+		if(Input.GetKey(KeyCode.D))
+		{
+			movementDirection += new Vector2(1,0);
+		}
+		movementDirection.Normalize();
+		if(Input.GetKey(KeyCode.LeftShift))
+		{
+			movementSpeed = moveSpeed * 2;
+		}
+		else
+		{
+			movementSpeed = moveSpeed;
+		}
+	
+		playerRigidbody.velocity = movementDirection * moveSpeed;
+	}
+
+	//Take Input
+	//Determine movement direction
+	//Multiply direction by movement Speed
+	//Apply Vector to player
+}
